e82e63cff3c8a3417c72f32c99650eaec1ad7118
Anna Elise Wong <anna.elise781@gmail.com>
2025-11-20 12:45:32 -0500

    Remove useless function creation

--- gemini_api_call.py
+++ gemini_api_call.py
@@ -24,7 +24,10 @@ def make_gemini_api_call(input_request: str) -> str:
 
     response = client.models.generate_content(
         model=GEMINI_MODEL,
-        contents=(input_request + " Make the response of this seem as though it was spoken by a human and not AI. Also please keep your response brief.")
+        contents=(
+            input_request
+            + " Make the response of this seem as though it was spoken by a human and not AI. Also please keep your response brief."
+        ),
     )
 
     client.close()
@@ -33,13 +36,7 @@ def make_gemini_api_call(input_request: str) -> str:
     return response.text
 
 
-## Upon trigger word, enter assistance:
-def gemini_chat_assistant() -> None:
-    """Create gemini chat assistant."""
-    pass
-
-
 if __name__ == "__main__":
-    while (True):
+    while True:
         input_request = input("What can I help you with? ")
         print(make_gemini_api_call(input_request))
